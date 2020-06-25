1ae66653cc2b446f0a76b733ba5454e0ca9627c2
Alexander Tolchinsky <bombur@kaap.io>
2020-06-25 14:00:13 +0300

    step6

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -15,3 +15,11 @@ test('compare YAML files', () => {
   expect(genDiff(absolutePathToFile1, absolutePathToFile2)).toEqual(correctAnswer);
   expect(genDiff('__fixtures__/before.yml', '__fixtures__/after.yml')).toEqual(correctAnswer);
 });
+
+test('compare INI files', () => {
+  const correctAnswer = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  - follow: false\n  + verbose: true\n}';
+  const absolutePathToFile1 = `${__dirname}/../__fixtures__/before.ini`;
+  const absolutePathToFile2 = `${__dirname}/../__fixtures__/after.ini`;
+  expect(genDiff(absolutePathToFile1, absolutePathToFile2)).toEqual(correctAnswer);
+  expect(genDiff('__fixtures__/before.ini', '__fixtures__/after.ini')).toEqual(correctAnswer);
+});
--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,19 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
+import * as ini from 'ini';
 
 const parse = (pathToFile) => {
   const absolutePathToFile = path.resolve(pathToFile);
   const fileContent = fs.readFileSync(absolutePathToFile, 'utf8');
-  const fileExtension = path.extname(pathToFile);
-  if (fileExtension === 'json') {
-    return JSON.parse(fileContent);
+  const fileExtension = path.extname(absolutePathToFile);
+  if (fileExtension === '.yml') {
+    return yaml.safeLoad(fileContent);
   }
-  return yaml.safeLoad(fileContent);
+  if (fileExtension === '.ini') {
+    return ini.parse(fileContent);
+  }
+  return JSON.parse(fileContent);
 };
 
 export default parse;
